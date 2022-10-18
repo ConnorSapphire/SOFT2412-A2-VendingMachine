3aa7251ff88c7b04615271ab3d03422e3b09c187
niye8576 <niye8576@uni.sydney.edu.au>
2022-10-19 09:29:29 +1100

    test

---
+++ app/src/test/java/VendingMachine/TransactionTest.java
@@ -0,0 +1,19 @@
+package VendingMachine;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+public class TransactionTest {
+
+    Transaction transaction;
+    Product products;
+
+    @Test 
+    void getStartTime(){
+        Date date = new Date();
+    }
+
+}

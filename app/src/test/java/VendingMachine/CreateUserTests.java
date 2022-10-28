cda87af04fe35999ff867179e9ae0d0d1d9d84bc
niye8576 <niye8576@uni.sydney.edu.au>
2022-10-28 12:44:01 +1100

    More Test

--- app/src/test/java/VendingMachine/CreateUserTests.java
+++ app/src/test/java/VendingMachine/CreateUserTests.java
@@ -1,8 +1,11 @@
 package VendingMachine;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
+
+import javax.annotation.meta.Exhaustive;
 
 import org.junit.jupiter.api.*;
 
@@ -14,7 +17,9 @@ public class CreateUserTests {
         HashMap<String, String> cards = new HashMap<>();
         cards.put("Name", "Number");
         UserCreator creator = new RegisteredCustomerCreator();
-        User customer = creator.create("Name", "Password", ui, cards);
+        User customer = creator.create(null, "Password", ui, cards);
+
+        customer.setUsername("Name");
 
         assertEquals("Name", customer.getUsername());
     }
@@ -25,7 +30,9 @@ public class CreateUserTests {
         HashMap<String, String> cards = new HashMap<>();
         cards.put("Name", "Number");
         UserCreator creator = new RegisteredCustomerCreator();
-        User customer = creator.create("Name", "Password", ui, cards);
+        User customer = creator.create("Name", null, ui, cards);
+
+        customer.setPassword("Password");
 
         assertEquals("Password", customer.getPassword());
     }
@@ -64,7 +71,7 @@ public class CreateUserTests {
     }
 
     @Test
-    void setGetUsers(){
+    void setGetUsersOwner(){
         UserInterface ui = new UserInterface(null);
         HashMap<String, User> users = new HashMap<>();
         
@@ -76,8 +83,54 @@ public class CreateUserTests {
 
     }
 
+    @Test
+    void storeNewCard(){
+        UserCreator creator = new OwnerCreator();
+        User customer = creator.create("Name", "Password", null, null);
+        customer.storeCard("CardName", "123456");
+
+        assertTrue(customer.isCardStored());
+    }
+
+    @Test
+    void getCardNameTest(){
+        UserCreator creator = new OwnerCreator();
+        User customer = creator.create("Name", "Password", null, null);
+        customer.storeCard("CardName", "123456");
+
+        assertEquals("CardName", customer.getCardName());
+    }
+
+    @Test
+    void getCardNumberTest(){
+        UserCreator creator = new OwnerCreator();
+        User customer = creator.create("Name", "Password", null, null);
+        customer.storeCard("CardName", "123456");
+
+        assertEquals("123456", customer.getCardNumber());
+    }
+
+    @Test 
+    void getAccessTest(){
+        UserCreator creator = new OwnerCreator();
+        User customer = creator.create("Name", "Password", null, null);
+        
+        customer.setAccessLevel("noAccess");
+        assertEquals("noAccess", customer.getAccessLevel());
+    }
+
+    @Test 
+    void transactionTest(){
+        UserCreator creator = new OwnerCreator();
+        User customer = creator.create("Name", "Password", null, null);
+        customer.storeCard("CardName", "123456");
+
+        assertEquals("123456", customer.getCardNumber());
+    }
 
 
 }
+
+
 
 

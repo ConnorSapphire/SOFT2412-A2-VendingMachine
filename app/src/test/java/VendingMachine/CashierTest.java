f241f77b37a68fdbd4bef8e96b6024edb7d7a8e7
unknown <xhua5250@uni.sydney.edu.au>
2022-11-02 08:39:13 +1100

    Cashier test

--- app/src/test/java/VendingMachine/CashierTest.java
+++ app/src/test/java/VendingMachine/CashierTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 
 public class CashierTest {
@@ -39,5 +40,95 @@ public class CashierTest {
         Change change = creator.create("$10", 10.0, 1);
         boolean filled = user.fillChange(change, 10);
         assertTrue(filled);
+    }
+
+    @Test
+    public void removeChangeNotExist(){
+        ChangeCreator creator = new NoteCreator();
+        Change change1 = creator.create("$10", 10.0, 1);
+        Change change2 = creator.create("$5", 5.0, 2);
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        all.put("$5", change2);
+        user.setChange(all);
+        boolean removed = user.removeChange(change1);
+        assertFalse(removed);
+    }
+
+    @Test
+    public void removeChangeTest(){
+        ChangeCreator creator = new NoteCreator();
+        Change change = creator.create("$10", 10.0, 1);
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        all.put("$10", change);
+        user.setChange(all);
+        boolean removed = user.removeChange(change);
+        assertTrue(removed);
+    }
+
+    @Test
+    public void addChangeNameExists(){
+        ChangeCreator creator = new NoteCreator();
+        Change change = creator.create("$10", 10.0, 1);
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        all.put("$10", change);
+        user.setChange(all);
+        boolean added = user.addChange("$10", 1, 10.0, "note");
+        assertFalse(added);
+    }
+
+    @Test
+    public void addChangeQuantityNegative(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", -1, 10.0, "note");
+        assertFalse(added);
+    }
+
+    @Test
+    public void addChangeQuantityZero(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", 0, 10.0, "note");
+        assertFalse(added);
+    }
+
+    @Test
+    public void addChangeValueNegative(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", 1, -1.0, "note");
+        assertFalse(added);
+    }
+
+    @Test
+    public void addChangeValueZero(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", 1, 0.0, "note");
+        assertFalse(added);
+    }
+
+    @Test
+    public void addChangeTypeInvalid(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", 1, 10.0, "hh");
+        assertFalse(added);
+    }
+
+    @Test
+    public void addChangeTypeCoin(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", 1, 10.0, "coin");
+        assertTrue(added);
+    }
+
+    @Test
+    public void addChangeTypeNote(){
+        LinkedHashMap<String, Change> all = new LinkedHashMap<>();
+        user.setChange(all);
+        boolean added = user.addChange("$10", 1, 10.0, "note");
+        assertTrue(added);
     }
 }
